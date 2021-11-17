86e7e06de39ae896258426a423642034a2e279b6
JeonGeunHee <weiding11086@gmail.com>
2021-11-17 10:53:59 +0900

    memo update

--- CordingTest_Grade2/땅따먹기.cpp
+++ CordingTest_Grade2/땅따먹기.cpp
@@ -153,6 +153,21 @@ int solution(vector<vector<int>> land)
 
 	return answer;
 }
+
+int Othersolution(vector<vector<int> > land)
+{
+	//기존의 vector를 임시 저장소로 이용하면서 더욱 간단하게 표현 한것.
+	//어차피 한 행의 원소 개수는 정해져 있으니까~
+	for (int i = 0; i < land.size() - 1; i++)
+	{
+		land[i + 1][0] += max(land[i][1], max(land[i][2], land[i][3]));
+		land[i + 1][1] += max(land[i][0], max(land[i][2], land[i][3]));
+		land[i + 1][2] += max(land[i][1], max(land[i][0], land[i][3]));
+		land[i + 1][3] += max(land[i][1], max(land[i][2], land[i][0]));
+	}
+
+	return *max_element(land[land.size() - 1].begin(), land[land.size() - 1].end());
+}
 #pragma endregion
 
 
